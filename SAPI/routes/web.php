3889b929a7fe52809fe50397dec46df476e2fc93
Juan <vjuandavid03@gmail.com>
2024-04-21 16:55:36 -0500

    Prueba

--- SAPI/routes/web.php
+++ SAPI/routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+    Route::get('/departamento',[DepartamentoController::class, 'index'])->name('departamento.index');
 });
