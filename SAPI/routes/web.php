2ff45b69ca57fc24c9c738a287ea53f14dd66a0f
Juan <vjuandavid03@gmail.com>
2024-04-25 19:52:06 -0500

    Ruta Protegida Clientes/Propiedades

--- SAPI/routes/web.php
+++ SAPI/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClientesController;
 use App\Http\Controllers\TransaccionesController;
+use App\Http\Controllers\PropiedadesController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -14,13 +15,21 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/Clientes',[ClientesController::class, 'index'])->name('Clientes.index');
-Route::post('/Clientes',[ClientesController::class, 'store'])->name('Clientes.store');
-Route::get('/Clientes/create', [ClientesController::class, 'create'])->name('Clientes.create');
-
+    Route::get('/clientes/create', [ClientesController::class, 'create'])->name('clientes.create');
+    Route::post('/clientes', [ClientesController::class, 'store'])->name('clientes.store');
+    Route::get('/clientes', [ClientesController::class, 'index'])->name('clientes.index');
+    Route::get('/clientes/{cliente}', [ClientesController::class, 'show'])->name('clientes.show');
+    Route::get('/clientes/{cliente}/edit', [ClientesController::class, 'edit'])->name('clientes.edit');
+    Route::put('/clientes/{cliente}', [ClientesController::class, 'update'])->name('clientes.update');
+    Route::delete('/clientes/{cliente}', [ClientesController::class, 'destroy'])->name('clientes.destroy');
+    //rutas propiedades
+    Route::get('/propiedades/create', [propiedadesController::class, 'create'])->name('propiedades.create');
+    Route::post('/propiedades', [propiedadesController::class, 'store'])->name('propiedades.store');
+    Route::get('/propiedades', [PropiedadesController::class, 'index'])->name('propiedades.index');
+    Route::get('/propiedades/{propiedad}', [propiedadesController::class, 'show'])->name('propiedades.show');
+    Route::get('/propiedades/{propiedad}/edit', [propiedadesController::class, 'edit'])->name('propiedades.edit');
+    Route::put('/propiedades/{propiedad}', [propiedadesController::class, 'update'])->name('propiedades.update');
+    Route::delete('/propiedades/{propiedad}', [propiedadesController::class, 'destroy'])->name('propiedades.destroy');
 
 
 Route::get('/Transacciones',[TransaccionesController::class, 'index'])->name('Transacciones.index');
