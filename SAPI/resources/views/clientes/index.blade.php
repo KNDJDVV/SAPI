a81fd96051c83c7acf7b74ed7402991f04e54fd6
Juan <vjuandavid03@gmail.com>
2024-04-26 20:03:06 -0500

    El Codigo el de verda el propio

--- SAPI/resources/views/clientes/index.blade.php
+++ SAPI/resources/views/clientes/index.blade.php
@@ -8,10 +8,10 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Listado de Clientes</title>
+    <title>Lista Clientes</title>
   </head>
   <body>
-    <h1>Listado de Clientes</h1>
+    
 
     <!-- Optional JavaScript; choose one of the two! -->
 
@@ -24,43 +24,64 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
   </body>
-  <body>
-    <div class="container">
-        <h1>Lista de Clientes</h1>
-        <a href="{{ route('Clientes.create')}}" class= "btn btn-sucess">Add</a>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Codigo</th>
+
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Clientes') }}
+        </h2>
+    </x-slot>
+    
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    <a href="{{ route('clientes.create') }}"
+                        class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded ml-2">Añadir</a>
+                    <table class="table">
+                        <thead>
+                            <tr>
+                            <th scope="col">id</th>
                     <th scope="col">Nombre</th>
-                    <th scope="col">Apellidos</th>
+                    <th scope="col">Apellido</th>
                     <th scope="col">Telefono</th>
                     <th scope="col">Email</th>
                     <th scope="col">Tipo</th>
                     <th scope="col">Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($clientes as $cliente)
-                <tr>
-                    <th scope="row">{{$cliente->id}}</th>
-                    <td>{{ $cliente->nombre }}</td>
-                    <td>{{ $cliente->apellido }}</td>
-                    <td>{{ $cliente->telefono }}</td>
-                    <td>{{ $cliente->email }}</td>
-                    <td>{{ $cliente->tipo }}</td>
-                    <td>
-                  
-                
-</form>
-                      
-                      
-                
-</form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-</table>
-  </body>
-</html>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        @foreach ($clientes as $cliente)
+                        <tr>
+                            <td>{{ $cliente->id }}</td>
+                            <td>{{ $cliente->nombre }}</td>
+                            <td>{{ $cliente->apellido }}</td>
+                            <td>{{ $cliente->telefono }}</td>
+                            <td>{{ $cliente->email }}</td>
+                            <td>{{ $cliente->tipo }}</td>
+                            <td>
+                            <a href="{{ route('clientes.edit', ['cliente' => $cliente->id]) }}"
+                                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                            Editar </a></li>
+
+
+                                            <form action="{{ route('clientes.destroy', ['cliente' => $cliente->id]) }}"
+                                    method="POST" style="display: inline-block">
+                                    @method('DELETE')
+                                    @csrf
+                                            <input
+                                                class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ml-2"
+                                                type="submit" value="Delete">
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
+
+
